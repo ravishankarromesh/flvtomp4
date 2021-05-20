d1a86a8cb10bf91d103410b15b788d057c28e1b9
unknown <ravishankar.romesh@unextt.com>
2021-05-20 14:00:52 +0530

    first commit

---
+++ convert/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+from django.http import JsonResponse
+import os
+import glob
+import boto3
+
+def flvtomp4(req):
+    os.system('aws s3 sync s3://new-image-profile /home/rohtash/flv --exclude "*" --include "*.flv"')
+    for name in glob.glob('/home/rohtash/flv/*'):
+        print(name)
+        os.system('ffmpeg -i '+name+' -f mp4 -vcodec mpeg4 -acodec libmp3lame /home/rohtash/mp4/'+name+'.mp4')
+        s3 = boto3.client('s3')
+        s3.upload_file(
+        '/home/rohtash/mp4/'+name+'.mp4', 'new-image-profile', name+'.mp4',
+        ExtraArgs={'ACL': 'public-read'}
+        )
+        os.system('rm /home/rohtash/mp4/'+name+'.mp4')
+        os.system('rm /home/rohtash/flv/'+name+'.mp4')
+
+    return JsonResponse({"msg":"success","data":"convert to mp4"})
