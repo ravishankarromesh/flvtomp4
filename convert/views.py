714df864f7706fb0034dd37ad93629e2bcc31617
ravishankarromesh <84492941+ravishankarromesh@users.noreply.github.com>
2021-05-21 10:00:20 +0530

    update view.py

--- convert/views.py
+++ convert/views.py
@@ -5,8 +5,12 @@ import os
 import glob
 import boto3
 import smtplib
-def flvtomp4(req):
-    os.system('aws s3 sync s3://new-image-profile /home/rohtash/flv --exclude "*" --include "*.flv"')
+
+def flvtomp4(req,d=''):
+    if(d != ''):
+        os.system('aws s3 sync s3://new-image-profile /home/rohtash/flv/'+d+'.flv')
+    else:
+        os.system('aws s3 sync s3://new-image-profile /home/rohtash/flv --exclude "*" --include "*.flv"')
     for name in glob.glob('/home/rohtash/flv/*'):
         print(name)
         os.system('ffmpeg -i '+name+' -f mp4 -vcodec mpeg4 -acodec libmp3lame /home/rohtash/mp4/'+name+'.mp4')
@@ -16,7 +20,8 @@ def flvtomp4(req):
         ExtraArgs={'ACL': 'public-read'}
         )
         os.system('rm /home/rohtash/mp4/'+name+'.mp4')
-        os.system('rm /home/rohtash/flv/'+name+'.mp4')
+        os.system('rm /home/rohtash/flv/'+name+'.flv')
+
         message = "submite"
         s = smtplib.SMTP('smtp.gmail.com',587)
         s.starttls()
@@ -24,4 +29,5 @@ def flvtomp4(req):
         s.sendmail("rraghav476@gmail.com","rraghav476@gmail.com",message)
         s.quit()
         print("msg send")
+
     return JsonResponse({"msg":"success","data":"convert to mp4"})
